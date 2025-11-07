609a601d8c072dd42b1781bc71c302a5b0ac32c8
kjmaster1 <kurtis.clements@hotmail.com>
2025-11-07 16:19:56 +0000

    Spawning Fix
    - Fix tutorial session not ending before spawn by explicitly waiting for it to end before spawning.

--- src/client/core.ts
+++ src/client/core.ts
@@ -1,6 +1,7 @@
 import {PlayerData} from "../common";
 import lib from "@communityox/ox_lib/client";
 import {destroyCharacterSelectionCam} from "./charselect";
+import {Delay} from "@nativewrappers/fivem";
 
 async function loadDefaultModel() {
   const model = 'mp_m_freemode_01';
@@ -36,8 +37,8 @@ class ClientCore {
 
       // Clean up the custom camera
       destroyCharacterSelectionCam();
-
       DoScreenFadeOut(500);
+      await Delay(500)
 
       if (data.isNew) {
         console.log('[Framework] New character, setting default model.');
@@ -50,10 +51,14 @@ class ClientCore {
         // For now, we just spawn.
       }
 
-
-      exports.spawnmanager.spawnPlayer();
-
       NetworkEndTutorialSession();
+
+      while (NetworkIsInTutorialSession()) {
+        await Delay(0);
+      }
+
+      console.log("[Framework] Tutorial session ended. Spawning player.");
+      exports.spawnmanager.spawnPlayer();
 
       const ped = PlayerPedId();
       SetEntityVisible(ped, true, true);
